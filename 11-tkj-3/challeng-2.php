31c7f46597b7ff183d14399ac3ff7ac4d960a463
williamtjandera956-arch <williamtjandera956@gmail.com>
2025-10-23 23:41:16 +0700

    Delete 11-tkj-3 directory

--- 11-tkj-3/challeng-2.php
+++
@@ -1,5 +0,0 @@
-<?php
-    
-    $listname = ["Membaca", "main game", "Berenang"];
-
-     echo"Hobi saya: $listname[0],$listname[1],$listname[2]";
--- 11-tkj-3/materi.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $nama= "Budi";
-    $umur="16";
-    
-    echo"Halo nama saya $nama dan saya umur $umur tahun";
